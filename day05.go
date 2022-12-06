964b3cb71aa8a8463d01be5933cf415d04250a6b
Austin Aurelio <austin@delveam.com>
2022-12-05 22:12:40 -0600

    Solve day 5 part 1

---
+++ day05.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func SolveDay05() {
+	content, err := os.ReadFile("input/day05.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	contentStr := string(content)
+
+	input := strings.Split(contentStr, "\n\n")
+
+	rawStacks := strings.Split(input[0], "\n")
+	instructions := strings.Split(input[1], "\n")
+
+	var stacks [][]string
+
+	stacks = append(stacks, make([]string, 0))
+
+	for _, s := range rawStacks {
+		var newStack []string
+		for _, c := range s {
+			newStack = append(newStack, string(c))
+		}
+
+		stacks = append(stacks, newStack)
+	}
+
+	for _, s := range instructions[:len(instructions)-1] {
+		tmp := strings.Split(s, " ")
+
+		move, _ := strconv.Atoi(tmp[1])
+		from, _ := strconv.Atoi(tmp[3])
+		to, _ := strconv.Atoi(tmp[5])
+
+		fromStack := &stacks[from]
+		toStack := &stacks[to]
+
+		removed := (*fromStack)[len(*fromStack)-move:]
+
+		for i, _ := range removed {
+			*toStack = append(*toStack, removed[len(removed)-1-i])
+		}
+
+		*fromStack = (*fromStack)[:len(*fromStack)-move]
+	}
+
+	fmt.Println(stacks)
+
+	for _, s := range stacks {
+		if len(s) > 0 {
+			fmt.Print(s[len(s)-1])
+		}
+	}
+}
